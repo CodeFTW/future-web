55ffee1ca4a9d65b869c704f6cfebcd871fdb31f
Jessé Alves <jessecarlos@gmail.com>
2017-10-24 14:31:31 -0200

    Adding service-worker

--- imports/ui/Home.js
+++ imports/ui/Home.js
@@ -4,8 +4,10 @@ import Card, { CardContent } from 'material-ui/Card';
 export default Home = () => {
     return (
         <Card>
-            <CardContent>
-                Template project using Meteor, React, Apollo, Material UI
+            <CardContent>                
+                <p style={{textAlign: 'justify'}}>
+                    <img src="/img/codeFTW.png" width="30" height="30"/> Template project using Meteor, React, Apollo, Material UI
+                </p>
             </CardContent>
         </Card>
     );
