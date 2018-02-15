be0c1c3adef98d473de4c755723af770131d3546
DenyHs <denilsonh2014@gmail.com>
2018-02-15 09:51:42 -0300

    Fixing the flash that occurs when the user refreshes the application and the application alternates between login and tasks

--- examples/todo/imports/containers/AppContainer.js
+++ examples/todo/imports/containers/AppContainer.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types';
 import { withContext, compose } from 'recompose';
 import { App } from '../ui/App';
 import { loggedUserQuery } from '../core/user/userQueries';
+import { displayLoadingState } from '../components/displayLoadingState';
 
 const data = loggedUserQuery;
 
@@ -11,4 +12,4 @@ const enhance = withContext(
   },
   ({ data: { loggedUser } }) => ({ loggedUser })
 );
-export const AppContainer = compose(data, enhance)(App);
+export const AppContainer = compose(data, displayLoadingState, enhance)(App);
