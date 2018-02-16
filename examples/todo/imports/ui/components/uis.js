46ff553afa7400f121937584bd43b96f5223c149
DenyHs <denilsonh2014@gmail.com>
2018-02-16 19:12:54 -0300

    Improving the updateAppTitle component

--- examples/todo/imports/ui/components/uis.js
+++ examples/todo/imports/ui/components/uis.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Tunnel } from 'react-tunnels';
 
-export const updateAppTitle = title => (
-  <Tunnel id="app-title">{title || 'Future Web'}</Tunnel>
-);
+export const updateAppTitle = title => {
+  if (document) {
+    document.title = title ? `${title} | Future Web` : 'Future Web';
+  }
+  return <Tunnel id="app-title">{title || 'Future Web'}</Tunnel>;
+};
