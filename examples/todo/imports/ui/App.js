f0591aac396accf43327a3faa00a80377a9ca196
filipe_nevola <filipe.nevola@tecsinapse.com.br>
2018-02-27 18:57:52 -0400

    delete component ConnectionStatus

--- examples/todo/imports/ui/App.js
+++ examples/todo/imports/ui/App.js
@@ -44,7 +44,6 @@ export const App = props => {
               <Typography type="headline" color="inherit">
                 <TunnelPlaceholder id="app-title" />
               </Typography>
-              <ConnectionStatus />
             </Toolbar>
           </AppBar>
           <Menu open={appState.open} onSelectMenu={closeMenu(props)} />
