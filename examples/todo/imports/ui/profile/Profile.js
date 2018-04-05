67e7472b366683c05d42105e0d1d030a88437377
Thiago Marinho <tgmarinho@gmail.com>
2018-04-05 14:22:27 -0400

    Change the Profile for Stateless and put the logic of Profile on ProfileContainer using HOC and withState

--- examples/todo/imports/ui/profile/Profile.js
+++ examples/todo/imports/ui/profile/Profile.js
@@ -1,56 +1,16 @@
 import React, { Fragment } from 'react';
-import { showAlert } from '@codeftw/future-web-ui-alert';
 import { Button, TextField } from 'material-ui';
 import { Save } from 'material-ui-icons';
 import { updateAppTitle } from '../components/uis';
 
   
-  handleSubmitProfile = () => !!this.state.firstName.trim();
 
-  // eslint-disable-next-line no-undef
-  editProfileAndGo = () => {
-    if (this.handleSubmitProfile()) {
-      const { editProfile, history, client } = this.props;
+export const Profile = ({editProfileState, onInputChange, editProfileAndGo}) => {
 
-      editProfile({
-        variables: {
-          user: {
-            ...this.state,
-          },
-        },
-      })
-        .then(() => {
-          // TODO when we have the cache working correctly after an update we can remove this
-          // https://github.com/CodeFTW/future-web/issues/34
-          client.resetStore();
-          showAlert('Profile updated');
-          history.push('/profile');
-        })
-        .catch(error => {
-          // eslint-disable-next-line no-console
-          console.log(error);
-        });
-    } else {
-      showAlert('The field Name is required');
-    }
-  };
-
-
-
-  
-
-export const Profile = ({user, setUser, loggedUser, onInputChange}) => {
-
-  console.log(user);
-
-  user = {...loggedUser};
-
-  const { email, firstName, lastName, age } = user; 
-  
-  // console.log(loggedUser);
-  console.log(user);
- 
+   const { email, firstName, lastName, age } = editProfileState; 
+   
     return (
+
       <Fragment>
         {updateAppTitle(`Edit Profile `)}
         <form className="form">
@@ -90,7 +50,7 @@ export const Profile = ({user, setUser, loggedUser, onInputChange}) => {
             className="form-action"
             raised
             color="primary"
-            onClick={this.editProfileAndGo}
+            onClick={editProfileAndGo}
           >
             <Save />
           </Button>
--- examples/todo/imports/ui/profile/ProfileContainer.js
+++ examples/todo/imports/ui/profile/ProfileContainer.js
@@ -3,8 +3,10 @@ import gql from 'graphql-tag';
 import { withRouter } from 'react-router-dom';
 import { compose, withState, withHandlers } from 'recompose';
 import { getLoggedUserContext } from '../../user/userContext';
+// import { withAlert } from '@codeftw/future-web-ui-alert';
 
 import { Profile } from './Profile';
+
 
 export const editProfileMutation = graphql(
   gql`
@@ -19,19 +21,58 @@ export const editProfileMutation = graphql(
   }
 );
 
+
 const loggedUser = getLoggedUserContext();
 
-const user = {email : '', firstName: '', lastName: '', age: 0};
+handleSubmitProfile = (editProfileState) => !!editProfileState.firstName.trim()
 
 export const ProfileContainer = compose(
-  withState('user', 'setUser', user),
-  withHandlers({
-    // eslint-disable-next-line no-undef
-    onInputChange : (props) => ({target: { name, value }}) => props.setUser({ ...props.user, [name] : value })
-  }),
-  
   editProfileMutation,
   loggedUser,
   withRouter,
-  withApollo
+  withApollo,
+  // withAlert,
+  withState('editProfileState', 'setEditProfileState', props =>
+    intialState = {
+      _id: props.loggedUser._id,
+      email: props.loggedUser.email,
+      firstName: props.loggedUser.firstName || '',
+      lastName: props.loggedUser.lastName || '',
+      age: props.loggedUser.age || 0,
+    },
+  ),
+  withHandlers({
+    // eslint-disable-next-line no-undef
+    onInputChange: (props) => ({ target: { name, value } }) =>
+      props.setEditProfileState({ ...props.editProfileState, [name]: value }),
+
+    editProfileAndGo: (props) => () => {
+      if (this.handleSubmitProfile(props.editProfileState)) {
+        const { editProfile, history, client } = props;
+        const { firstName, lastName, age } = props.editProfileState;
+        editProfile({
+          variables: {
+            user: {
+              firstName, lastName, age
+            },
+          },
+        })
+          .then(() => {
+            // TODO when we have the cache working correctly after an update we can remove this
+            // https://github.com/CodeFTW/future-web/issues/34
+            client.resetStore();
+            // TODO: I don't know how can I show the alert for user
+            console.log('Profile updated');
+            history.push('/profile');
+          })
+          .catch(error => {
+            // eslint-disable-next-line no-console
+            console.log(error);
+          });
+      } else {
+         // TODO: I don't know how can I show the alert for user
+         console.log('Profile updated');
+      }
+    }
+  })
 )(Profile);
