bc1b027604ef8d8870f7b64a267a671561bb64cd
Thiago Marinho <tgmarinho@gmail.com>
2018-04-12 12:22:45 -0400

    refactoring the code and separating into methods

--- examples/todo/imports/ui/profile/ProfileContainer.js
+++ examples/todo/imports/ui/profile/ProfileContainer.js
@@ -21,56 +21,64 @@ export const editProfileMutation = graphql(
 
 const loggedUser = getLoggedUserContext();
 
-const handleSubmitProfile = editProfileState =>
-  !!editProfileState.firstName.trim();
-
-export const ProfileContainer = compose(
-  editProfileMutation,
-  loggedUser,
-  withRouter,
-  withApollo,
-  withState('editProfileState', 'setEditProfileState', props => ({
+const initEditProfileState = (props) => 
+  ({
     _id: props.loggedUser._id,
     email: props.loggedUser.email,
     firstName: props.loggedUser.firstName || '',
     lastName: props.loggedUser.lastName || '',
     age: props.loggedUser.age || 0,
     gender: props.loggedUser.gender || 'male',
-  })),
-  withHandlers({
-    // eslint-disable-next-line no-undef
-    onInputChange: props => ({ target: { name, value } }) =>
-      props.setEditProfileState({ ...props.editProfileState, [name]: value }),
+  });
 
-    editProfileAndGo: props => () => {
-      if (handleSubmitProfile(props.editProfileState)) {
-        const { editProfile, history, client } = props;
-        const { firstName, lastName, age } = props.editProfileState;
-        editProfile({
-          variables: {
-            user: {
-              firstName,
-              lastName,
-              age,
-            },
-          },
-        })
-          .then(() => {
-            // TODO when we have the cache working correctly after an update we can remove this
-            // https://github.com/CodeFTW/future-web/issues/34
-            client.resetStore();
-            // TODO: I don't know how can I show the alert for user
-            showAlert('Profile updated');
-            history.push('/profile');
-          })
-          .catch(error => {
-            // eslint-disable-next-line no-console
-            console.log(error);
-          });
-      } else {
+
+const handleSubmitProfile = editProfileState =>
+  !!editProfileState.firstName.trim();
+
+const actionSave = props => {
+  if (handleSubmitProfile(props.editProfileState)) {
+    const { editProfile, history, client } = props;
+    const { firstName, lastName, age } = props.editProfileState;
+    editProfile({
+      variables: {
+        user: {
+          firstName,
+          lastName,
+          age,
+        },
+      },
+    })
+      .then(() => {
+        // TODO when we have the cache working correctly after an update we can remove this
+        // https://github.com/CodeFTW/future-web/issues/34
+        client.resetStore();
         // TODO: I don't know how can I show the alert for user
         showAlert('Profile updated');
-      }
-    },
+        history.push('/profile');
+      })
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      });
+  } else {
+    // TODO: I don't know how can I show the alert for user
+    showAlert('Profile updated');
+  }
+}  
+
+const editValues = props => ({ target: { name, value } }) => {
+    props.setEditProfileState({ ...props.editProfileState, [name]: value })
+}
+
+export const ProfileContainer = compose(
+  editProfileMutation,
+  loggedUser,
+  withRouter,
+  withApollo,
+  withState('editProfileState', 'setEditProfileState', props => initEditProfileState(props)),
+  withHandlers({
+    // eslint-disable-next-line no-undef
+    onInputChange: props => editValues(props),
+    editProfileAndGo: props => () => actionSave(props)
   })
 )(Profile);
